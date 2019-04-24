3718f85a67b327dabdf954a0f5747228102fc07d
Trenton Parrotte <trenton.parrotte@du.edu>
2019-04-23 18:44:37 -0600

    Added project

---
+++ ReligiousApp/ReligiousApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReligiousApp
+//
+//  Created by Trenton Parrotte on 4/23/19.
+//  Copyright © 2019 Trenton Parrotte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
